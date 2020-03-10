8843cf5e8e8901125c3d221d8aaff75367fa656c
Eric Masiello <eric.j.masiello@gmail.com>
2020-03-10 16:26:28 -0400

    Update to latest rolllbar plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
-import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+// A Rollup plugin to convert CommonJS modules to ES6, so they can be
+// included in a Rollup bundle
+import commonjs from '@rollup/plugin-commonjs';
+// Automatically externalize peerDependencies in a rollup bundle.
+// Also optionally externalize dependencies in a rollup bundle.
 import external from 'rollup-plugin-peer-deps-external';
+// Integration between Rollup and PostCSS.
 import postcss from 'rollup-plugin-postcss';
-import resolve from 'rollup-plugin-node-resolve';
+//  A Rollup plugin which locates modules using the Node resolution
+// algorithm, for using third party modules in node_modules
+import resolve from '@rollup/plugin-node-resolve';
 import autoprefixer from 'autoprefixer';
 
 import pkg from './package.json';
